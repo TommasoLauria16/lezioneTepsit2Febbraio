5a222d71bd04b34eaac9876a4e6bf2d2b5b53bc0
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-02 12:42:38 +0100

    Add project files.

---
+++ EsercitazioneTest/Operazioni.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibreriaEsercitazione;
+
+namespace EsercitazioneTest
+{
+    [TestClass]
+    public class Operazioni
+    {
+        [TestMethod]
+        public void TestCalcoloMisterioso()
+        {
+            int n = 5;
+            long valore_aspettato = 240;
+            long resp = GestioneTest.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        [TestMethod]
+        public void TestCalcoloMisterioso_1()
+        {
+            int n = 2;
+            long valore_aspettato = 4;
+            long resp = GestioneTest.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        [TestMethod]
+        public void TestMisurazioneMisteriosa()
+        {
+            double[] valori = new double[] { 4, 6, 7, -9.1 };
+            double min = GestioneTest.MisuraMistariosa(valori);
+            double valore_aspettato = 3;
+            Assert.AreEqual(valore_aspettato,min);
+        }
+        [TestMethod]
+        public void TestMisurazioneMisteriosa_1()
+        {
+            double[] valori = new double[] { 4, 6, 7, -9.1 };
+            double min = GestioneTest.MisuraMistariosa1(valori);
+            double valore_aspettato = -9.1;
+            Assert.AreEqual(valore_aspettato, min);
+        }
+        [TestMethod]
+        public void TestCalcoloMisterioso2()
+        {
+            long n = 3;
+            long valore_aspettato = -18;
+            long resp = GestioneTest.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+    }
+}
---
+++ LibreriaEsercitazione/GestioneTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LibreriaEsercitazione
+{
+    public class GestioneTest
+    {
+        public static long CalcoloMisterioso(int n)
+        {
+            long valore = 2;
+            if (n == 0)
+                valore = 1;
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                    valore *= i;
+            }
+            return valore;
+        }
+        public static double MisuraMistariosa(double[] valori)
+        {
+            double a=0;
+            for (int i=0;i<valori.Length;i++)
+            {
+                if (valori[i] < a)
+                    a = i;
+            }
+            return a;
+        }
+        public static double MisuraMistariosa1(double[] valori)
+        {
+            double a = 0;
+            for (int i = 0; i < valori.Length; i++)
+            {
+                if (valori[i] < a)
+                    a =valori[i];
+            }
+            return a;
+        }
+        public static long CalcoloMisterioso2(long n)
+        {
+
+            long valore = -3;
+            if (n == 0)
+                valore = 0;
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                    valore = valore * i;
+            }
+
+            return valore;
+        }
+
+    }
+}
